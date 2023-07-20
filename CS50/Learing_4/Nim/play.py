f50c5ee69975c2bf116950088f2a40638b63b885
Lai lipeng <100109981+lihuapeng777@users.noreply.github.com>
2023-07-20 12:10:47 +0800

    Add files via upload

---
+++ CS50/Learing_4/Nim/play.py
@@ -0,0 +1,11 @@
+from nim import train, play
+
+#指定训练次数
+ai = train(0)
+
+'''
+0为玩家先手
+1为ai先手
+'''
+
+play(ai, 0)
---
+++ CS50/Learing_4/Nim/test.py
@@ -0,0 +1,13 @@
+class A(object):
+    bar = 1
+
+    def func1(self):
+        print("foo")
+    
+    @classmethod
+    def func2(cls):
+        print("func2")
+        print(cls.bar)
+        cls().func1()
+
+A.func2()
